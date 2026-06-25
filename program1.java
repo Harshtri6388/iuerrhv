28bb47549f40fd7b77b999759d00d6eb08e4cc22
Harshtri6388 <tripathiharsh6388300@gmail.com>
2026-06-25 13:23:34 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,17 @@
+public class program1 {
+    public static void main(String[] args) {
+        //for(int i=0; i<11 ;i=i+1){
+        //    System.out.println(i);
+        //} 
+        int i=0;       
+        //while(i<11){
+        //    System.out.println(i+"");
+        //    i++;
+       // }
+       do { 
+           System.out.println(i);
+           i++;
+       } while (i<11);
+    }
+    
+}
